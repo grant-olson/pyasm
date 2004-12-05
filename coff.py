c6e4e59e98d9a7d46688bf04f5abf7492acf924c
Grant Olson <kgo@grant-olson.net>
2004-12-05 03:27:40 +0000

    initial checkin

---
+++ coff.py
@@ -0,0 +1,436 @@
+import struct, sys
+from coffConst import *
+
+def pylongToSignedInt(x):
+    if x > 0xFFFFFFFF:
+        raise TypeError("This is too big to convert to a signed int")
+    elif x >= 0x80000000:
+        x = x - sys.maxint - sys.maxint - 2
+    else:
+        pass
+    return int(x)
+
+def ucharFromFile(f):return struct.unpack("<B", f.read(1))[0]
+def ushortFromFile(f):return struct.unpack("<H", f.read(2))[0]
+def shortFromFile(f):return struct.unpack("<h", f.read(2))[0]
+def ulongFromFile(f): return struct.unpack("<L", f.read(4))[0]
+
+def stringFromFile(n,f):return struct.unpack("<%ds" % n, f.read(n))[0]
+
+def ucharToFile(f,u): f.write(struct.pack("<B", u))
+def ushortToFile(f, u): f.write(struct.pack("<H", u))
+def shortToFile(f, u): f.write(struct.pack("<h", u))
+def ulongToFile(f, u): f.write(struct.pack("<L", u))
+def stringToFile(f, n, u): f.write(struct.pack("<%ds" % n, u))
+
+# These are a bit wierd because of python's use of longs with
+# bitwise operations
+def longFromFile(f): return struct.unpack("<L", f.read(4))[0]
+def longToFile(f, u): f.write(struct.pack("<i", pylongToSignedInt(u)))
+
+def attemptNameLookup(const,id): return const.get(id, "UNDEF??[%0X]" % id)
+
+def charsAsBinDump(chars, address = 0):
+    head,tail = chars[:16], chars[16:]
+    while head:
+        print "%08X:  " % address ,
+        address += 0x10
+        for char in head:
+            print "%02X" % ord(char) ,
+        if len(head) < 16:
+            for i in range(16-len(head)):
+                print "  " ,
+        print "   ",
+        for char in head:
+            if ord(char)<=32:
+                sys.stdout.write(".")
+            else:
+                sys.stdout.write(char)
+        print
+        head,tail = tail[:16], tail[16:]
+
+class coffSymbolEntry:
+    def __init__(self,name="",value=0x0,sec=0x0,typ=0x0,cls=0x0):
+        self.Name = name
+        self.Value = value
+        self.SectionNumber = sec
+        self.Type = typ
+        self.StorageClass = cls
+        self.NumberAuxiliary = 0x0
+        self.Auxiliaries = ''
+
+    def InitFromFile(self,f):
+        self.Name = stringFromFile(8,f)
+        self.Value = ulongFromFile(f)
+        self.SectionNumber = shortFromFile(f)
+        self.Type = ushortFromFile(f)
+        self.StorageClass = ucharFromFile(f)
+        self.NumberAuxiliary = ucharFromFile(f)
+        self.Auxiliaries = ''
+
+        for i in range(self.NumberAuxiliary):
+            aux = stringFromFile(18,f)
+            self.Auxiliaries += aux
+
+
+    def WriteToFile(self,f):
+        stringToFile(f, 8, self.Name)
+        ulongToFile(f, self.Value)
+        shortToFile(f, self.SectionNumber)
+        ushortToFile(f, self.Type)
+        ucharToFile(f, self.StorageClass)
+        ucharToFile(f, self.NumberAuxiliaries)
+        stringToFile(f, len(self.Auxiliaries), self.Auxiliaries)
+
+    def SetSizes(self):
+        assert not len(self.Auxiliaries) % 18, "Invalid Aux length"
+        self.NumberAuxiliaries =  (len(self.Auxiliaries) // 18)
+    def Rows(self):
+        return self.NumberAuxiliaries + 1
+
+    def DumpInfo(self):
+        print "%20s\t%10s\t%10s\t%10s\t%10s\t" % (repr(self.Name),
+                    attemptNameLookup(SymbolValues.NAME,self.Value),
+                    repr(self.SectionNumber),
+                    attemptNameLookup(SymbolTypes.NAME,self.Type),
+                    attemptNameLookup(SymbolClass.NAME,self.StorageClass))
+        tail = repr(self.Auxiliaries)
+        head,tail = tail[:70],tail[70:]
+        while head:
+            print "\t%s" % head
+            head,tail = tail[:70],tail[70:]
+        
+class coffSymbolList(list):
+    def DumpInfo(self):
+        if self:
+            print "Symbol Entry Table"
+            print "=================="
+            print "%20s\t%10s\t%10s\t%10s\t%10s\t" % ("Name",
+                        'Value','SectionNumber','Type','StorageClass')
+        for x in self:
+            x.DumpInfo()
+            
+    def InitFromFile(self, f, count):
+        x = 0
+        while x < count:
+            symbol = coffSymbolEntry()
+            symbol.InitFromFile(f)
+            x += 1 + symbol.NumberAuxiliary #account for aux stubs
+            self.append(symbol)          
+
+    def WriteToFile(self, f):
+        for sym in self:
+            sym.WriteToFile(f)
+            
+class coffLineNumberEntry:
+    def __init__(self,sym=0x0,num=0x0):
+        self.Symbol = sym
+        self.Number = num
+
+    def InitFromFile(self,f):
+        self.Symbol = ulongFromFile(f)
+        self.Number = ushortFromFile(f)
+
+    def WriteToFile(self,f):
+        ulongToFile(f, self.Symbol)
+        ushortToFile(f,self.Number)
+        
+    def DumpInfo(self):
+        print "%s\t%s" % (self.Symbol, self.Number)
+
+    def Sizeof(self):
+        return 6
+    
+class coffLineNumberList(list):
+    def DumpInfo(self):
+        if self:
+            print "LINE NUMBERS"
+            print "============"
+            print "Symbol\tLine Number"
+        for x in self:
+            x.DumpInfo()
+            
+class coffRelocationEntry:
+    def __init__(self,addr=0x0,sym=0x0,typ=0x0):
+        self.Address = addr
+        self.Symbol = sym
+        self.Type = typ
+
+    def InitFromFile(self,f):
+        self.Address = ulongFromFile(f)
+        self.Symbol = ulongFromFile(f)
+        self.Type = ushortFromFile(f)
+
+    def WriteToFile(self,f):
+        ulongToFile(f,self.Address)
+        ulongToFile(f, self.Symbol)
+        ushortToFile(f, self.Type)
+        
+    def Sizeof(self):
+        return 10
+    
+    def DumpInfo(self):
+        print "%02X\t%02X\t%10s" % (self.Address, self.Symbol,
+                              attemptNameLookup(RelocationTypes.NAME, self.Type))
+
+class coffRelocationList(list):
+    def DumpInfo(self):
+        if self:
+            print "Relocation Data"
+            print "==============="
+            print "%08s\t%04s\t%10s" % ("Address","Symbol","Type")
+        for x in self:
+            x.DumpInfo()
+            
+class coffSection:
+    def __init__(self):
+        self.Name = ""
+        self.PhysicalAddress = 0x0
+        self.VirtualAddress = 0x0
+        self.RawDataSize = 0x0
+        self.RawDataLoc = 0x0
+        self.RelocationLoc = 0x0
+        self.LineNumberLoc = 0x0
+        self.RelocationCount = 0x0
+        self.LineNumberCount = 0x0
+        self.Flags = 0x0
+        self.RawData = ""
+        self.LineNumberData = coffLineNumberList()
+        self.RelocationData = coffRelocationList()
+
+    def InitFromFilePass1(self, f):
+        self.Name = stringFromFile(8,f)
+        self.PhysicalAddress = ulongFromFile(f)
+        self.VirtualAddress = ulongFromFile(f)
+        self.RawDataSize = ulongFromFile(f)
+        self.RawDataLoc = ulongFromFile(f)
+        self.RelocationLoc = ulongFromFile(f)
+        self.LineNumberLoc = ulongFromFile(f)
+        self.RelocationCount = ushortFromFile(f)
+        self.LineNumberCount = ushortFromFile(f)
+        self.Flags = longFromFile(f)
+
+    def WriteToFilePass1(self, f):
+        stringToFile(f,8,self.Name)
+        ulongToFile(f,self.PhysicalAddress)
+        ulongToFile(f,self.VirtualAddress)
+        ulongToFile(f,self.RawDataSize)
+        ulongToFile(f,self.RawDataLoc)
+        ulongToFile(f,self.RelocationLoc)
+        ulongToFile(f,self.LineNumberLoc)
+        ushortToFile(f,self.RelocationCount)
+        ushortToFile(f,self.LineNumberCount)
+        longToFile(f,self.Flags)
+        
+    def InitFromFilePass2(self, f):      
+        if self.RawDataSize:
+            assert self.RawDataLoc == f.tell(), "Out of Sync"
+            self.RawData = stringFromFile(self.RawDataSize, f)
+        if self.RelocationCount:
+            assert self.RelocationLoc == f.tell(), "Out of Sync"
+            for i in range(self.RelocationCount):
+                relEnt = coffRelocationEntry()
+                relEnt.InitFromFile(f)
+                self.RelocationData.append(relEnt)
+        if self.LineNumberCount:
+            assert self.LineNumberLoc == f.tell(), "Out of Sync"
+            for i in range(self.LineNumberCount):
+                ln = coffLineNumberEntry()
+                ln.InitFromFile(f)
+                self.LineNumberData.append(ln)
+                
+    def WriteToFilePass2(self,f):
+        if self.RawDataSize:
+            stringToFile(f, self.RawDataSize,self.RawData)
+        if self.RelocationCount:
+            for rec in self.RelocationData:
+                rec.WriteToFile(f)
+        if self.LineNumberCount:
+            for ln in self.LineNumberData:
+                ln.WriteToFile(f)
+    
+    def Sizeof(self):
+        "excluding data"
+        return 40
+
+    def SetSizes(self):
+        self.RawDataSize = len(self.RawData)
+        self.RelocationCount = len(self.RelocationData)
+        self.LineNumberCount = len(self.LineNumberData)
+
+    def SetOffsets(self, currentOffset):
+        self.RawDataLoc = currentOffset
+        currentOffset += self.RawDataSize
+        if self.RelocationCount:
+            self.RelocationLoc = currentOffset
+            currentOffset += self.RelocationCount * 10
+        else:
+            self.RelocationLoc = 0x0
+            
+        if self.LineNumberCount:
+            self.LineNumberLoc = currentOffset
+            currentOffset += self.LineNumberCount * 6
+        else:
+            self.LineNumberLoc = 0x0
+        return currentOffset
+        
+    def DumpInfo(self, showData=True):
+        print
+        print "SECTION"
+        print "======="
+        print
+        print "Name %s\n" % self.Name
+        print "PhysicalAddress %s" % self.PhysicalAddress
+        print "VirtualAddress %s" % self.VirtualAddress
+        print "RawDataSize %s" % self.RawDataSize
+        print "RawDataLoc %s" % self.RawDataLoc
+        print "RelocationTable %s" % self.RelocationLoc
+        print "LineNumberTable %s" % self.LineNumberLoc
+        print "RelocationCount %s" % self.RelocationCount
+        print "LineNumberCount %s" % self.LineNumberCount
+        print "Flags %s" % self.Flags
+        for key in SectionFlags.NAME.keys():
+            if self.Flags & key:
+                print "\t%s" % SectionFlags.NAME[key]
+        align = self.Flags & SectionFlags.ALIGN_MASK
+        print "\t%s" % SectionFlags.ALIGN_NAME[align]
+        print
+
+        if showData:
+            charsAsBinDump(self.RawData)
+            self.RelocationData.DumpInfo()
+            self.LineNumberData.DumpInfo()
+            print
+        
+class coffFile:
+    I386MAGIC = 0x14c
+
+    NO_RELOC = 0x1
+    EXECUTABLE = 0x2
+    NO_LINENO = 0x4
+    NO_SYMBOLS = 0x8
+    LITTLEENDIAN = 0x100
+    NO_DEBUG = 0x200
+    SYSTEM = 0x1000
+    DLL = 0x2000
+
+    FLAG_NAMES = {NO_RELOC: 'NO_RELOC' ,
+                  EXECUTABLE: 'EXECUTABLE',
+                  NO_LINENO: 'NO_LINENO',
+                  NO_SYMBOLS: 'NO_SYMBOLS',
+                  LITTLEENDIAN: 'LITTLENDIAN',
+                  NO_DEBUG: 'NO DEBUG',
+                  SYSTEM: 'SYSTEM',
+                  DLL: 'DLL'}
+    
+    def __init__(self):
+        self.MachineType = 0x0
+        self.NumberOfSections = 0x0
+        self.Timestamp = 0x0
+        self.SymbolTableLoc = 0x0
+        self.SymbolCount = 0x0
+        self.Symbols = coffSymbolList()
+        self.OptionalHeaderSize = 0x0
+        self.Characteristics = 0x0
+        self.Sections = []
+        self.StringTableSize = 0
+        self.StringTable = ""
+
+    def InitFromFile(self, f):
+        self.MachineType = ushortFromFile(f)
+        self.NumberOfSections = ushortFromFile(f)
+        self.Timestamp = ulongFromFile(f)
+        self.SymbolTableLoc = ulongFromFile(f)
+        self.SymbolCount = ulongFromFile(f)
+        self.OptionalHeaderSize = ushortFromFile(f)
+        self.Characteristics = ushortFromFile(f)
+        for i in range(self.NumberOfSections):
+            sec = coffSection()
+            sec.InitFromFilePass1(f)
+            self.Sections.append(sec)
+        for sec in self.Sections:
+            sec.InitFromFilePass2(f)
+        assert self.SymbolTableLoc == f.tell(), "Out of sync"
+        self.Symbols.InitFromFile(f,self.SymbolCount)
+        self.StringTableSize = ulongFromFile(f) - 4 #includes itself in the count
+        self.StringTable = f.read(self.StringTableSize)
+
+        if f.read():
+            raise Exception("Finished processing before end of file")
+
+    def WriteToFile(self, f):        
+        self.SetSizes()
+        self.SetOffsets()
+        
+        ushortToFile(f,self.MachineType)
+        ushortToFile(f,self.NumberOfSections)
+        ulongToFile(f,self.Timestamp)
+        ulongToFile(f,self.SymbolTableLoc)
+        ulongToFile(f,self.SymbolCount)
+        ushortToFile(f,self.OptionalHeaderSize)
+        ushortToFile(f,self.Characteristics)
+        for sec in self.Sections:
+            sec.WriteToFilePass1(f)
+        for sec in self.Sections:
+            sec.WriteToFilePass2(f)
+        self.Symbols.WriteToFile(f)
+
+        ulongToFile(f, len(self.StringTable)+4)
+        stringToFile(f, len(self.StringTable), self.StringTable)
+
+    def Sizeof(self):
+        "header only"
+        return 20
+
+    def SetSizes(self):
+        for sec in self.Sections:
+            sec.SetSizes()
+        self.NumberOfSections = len(self.Sections)
+        self.SymbolCount = 0
+        for sym in self.Symbols:
+            sym.SetSizes()
+            self.SymbolCount += sym.Rows()
+
+    def SetOffsets(self):
+        offset = self.Sizeof()
+        offset += len(self.Sections) * 40
+        i = 1
+        for sec in self.Sections:
+            tmpOffset = offset
+            offset = sec.SetOffsets(offset)
+            assert tmpOffset == sec.RawDataLoc, "section %s data out of sync" % i
+            i += 1
+        self.SymbolTableLoc = offset
+        
+
+    def DumpInfo(self):
+        print "Machine Type: %s" % self.MachineType
+        print "Number of sections: %s" % self.NumberOfSections
+        print "DateTime %s" % self.Timestamp
+        print "Pointer to symbol table %s" % self.SymbolTableLoc
+        print "Number of symbols %s" % self.SymbolCount
+        print "Optional Header size %s" % self.OptionalHeaderSize
+        print "Characteristics: %s" % self.Characteristics
+        if self.Characteristics & coffFile.NO_RELOC:print "\tNO RELCATION INTO"
+        if self.Characteristics & coffFile.EXECUTABLE: print"\tEXECUTABLE"
+        if self.Characteristics & coffFile.NO_LINENO: print "\tNO LINE NOs"
+        if self.Characteristics & coffFile.NO_SYMBOLS: print "\tNO SYMBOLS"
+        if self.Characteristics & coffFile.LITTLEENDIAN: print "\tLITTLEENDIAN"
+
+        for sec in self.Sections:
+            sec.DumpInfo()
+        self.Symbols.DumpInfo()
+        print "String Table %s" % repr(self.StringTable)
+
+if __name__ == "__main__":      
+    f = file("C:/objtest/objtest/Release/objtest.obj","rb")
+
+    coff = coffFile()
+    coff.InitFromFile(f)
+    f.close()
+
+    coff.DumpInfo()
+
+    f2 = file("C:/objtest/objtest/Release/objtest.obj2","wb")
+    coff.WriteToFile(f2)
+    f2.close()
---
+++ test/rawHelloWorld.py
@@ -0,0 +1,110 @@
+from coff import coffFile, coffSection, coffRelocationEntry, coffSymbolEntry
+from coffConst import *
+import time
+
+c = coffFile()
+
+c.MachineType = coffFile.I386MAGIC
+
+s1 = coffSection()
+s1.Name = ".drectve"
+s1.Flags = (SectionFlags.LNK_REMOVE |
+            SectionFlags.LNK_INFO |
+            SectionFlags.ALIGN_1BYTES)
+s1.RawData = '-defaultlib:LIBC -defaultlib:OLDNAMES '
+c.Sections.append(s1)
+
+s2 = coffSection()
+s2.Name = ".text\x00\x00\x00"
+s2.Flags = (SectionFlags.CNT_CODE |
+            SectionFlags.LNK_COMDAT |
+            SectionFlags.MEM_EXECUTE |
+            SectionFlags.MEM_READ |
+            SectionFlags.ALIGN_16BYTES)
+s2.RawData = "\x68\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x83\xC4\x04\x33\xC0\xC3"
+
+r = coffRelocationEntry(addr=0x1,sym=0xC,typ=RelocationTypes.I386_DIR32)
+s2.RelocationData.append(r)
+
+r= coffRelocationEntry(addr=0x6,sym=0x9,typ=RelocationTypes.I386_REL32)
+s2.RelocationData.append(r)
+
+c.Sections.append(s2)
+
+
+s3 = coffSection()
+s3.Name = '.data\x00\x00\x00'
+s3.Flags = (SectionFlags.LNK_COMDAT |
+            SectionFlags.CNT_INITIALIZED_DATA |
+            SectionFlags.MEM_WRITE |
+            SectionFlags.MEM_READ |
+            SectionFlags.ALIGN_4BYTES)
+s3.RawData = '\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21\x0a\x00'
+c.Sections.append(s3)
+
+s4=coffSection()
+s4.Name = '.debug$F'
+s4.Flags = (SectionFlags.LNK_COMDAT |
+            SectionFlags.TYPE_NO_PAD |
+            SectionFlags.CNT_INITIALIZED_DATA |
+            SectionFlags.MEM_DISCARDABLE |
+            SectionFlags.MEM_READ |
+            SectionFlags.ALIGN_1BYTES)
+s4.RawData = '\x00\x00\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00'
+
+r = coffRelocationEntry(addr=0x0,sym=0x8,typ=RelocationTypes.I386_DIR32NB)
+s4.RelocationData.append(r)
+
+c.Sections.append(s4)
+
+s = coffSymbolEntry('.file\x00\x00\x00',SymbolValues.SYM_UNDEFINED,-2,
+                    SymbolTypes.NULL, SymbolClass.CLASS_FILE)
+s.Auxiliaries = 'C:\\objtest\\objtest\\objtest.cpp\x00\x00\x00\x00\x00\x00'
+c.Symbols.append(s)
+
+s = coffSymbolEntry('@comp.id',0xB2306, -1, SymbolTypes.NULL, SymbolClass.STATIC)
+c.Symbols.append(s)
+
+s = coffSymbolEntry('.drectve', SymbolValues.SYM_UNDEFINED, 1, SymbolTypes.NULL,
+                    SymbolClass.STATIC)
+s.Auxiliaries = '&\x00\x00\x00\x00\x00\x00\x00O\xe0\xad\x98\x00\x00\x00\x00\x00\x00'
+c.Symbols.append(s)
+
+s = coffSymbolEntry('.text\x00\x00\x00', SymbolValues.SYM_UNDEFINED, 2,
+                    SymbolTypes.NULL, SymbolClass.STATIC)
+s.Auxiliaries = "\x10\x00\x00\x00\x02\x00\x00\x00\x9d\xf0\xcd3\x00\x00\x01\x00\x00\x00"
+c.Symbols.append(s)
+
+s = coffSymbolEntry('_main\x00\x00\x00', SymbolValues.SYM_UNDEFINED, 2, 0x20,
+                    SymbolClass.EXTERNAL)
+c.Symbols.append(s)
+
+s = coffSymbolEntry('_printf\x00', SymbolValues.SYM_UNDEFINED, 0, 0x20,
+                    SymbolClass.EXTERNAL)
+c.Symbols.append(s)
+
+s = coffSymbolEntry('.data\x00\x00\x00', SymbolValues.SYM_UNDEFINED, 3,
+                    SymbolTypes.NULL, SymbolClass.STATIC)
+s.Auxiliaries = '\x0e\x00\x00\x00\x00\x00\x00\x00\xfe,\xa6\xfb\x00\x00\x02\x00\x00\x00'
+c.Symbols.append(s)
+
+s = coffSymbolEntry('\x00\x00\x00\x00\x04\x00\x00\x00', SymbolValues.SYM_UNDEFINED, 3,
+                    SymbolTypes.NULL, SymbolClass.EXTERNAL)
+c.Symbols.append(s)
+
+s = coffSymbolEntry('.debug$F', SymbolValues.SYM_UNDEFINED, 4, SymbolTypes.NULL,
+                    SymbolClass.STATIC)
+s.Auxiliaries = '\x10\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x02\x00\x05\x00\x00\x00'
+c.Symbols.append(s)
+
+c.StringTable = '??_C@_0O@FEEI@Hello?5World?$CB?6?$AA@\x00'
+
+c.SetSizes()
+c.SetOffsets()
+
+c.DumpInfo()
+
+f = file("C:/objtest/objtest/Release/objtest.obj3","wb")
+c.WriteToFile(f)
+f.close()
+
