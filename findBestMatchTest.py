e1ac65a4aaebb7a27ce38857d523abfb18cca3dd
Grant Olson <kgo@grant-olson.net>
2005-02-08 04:03:09 +0000

    This is a scratch area to prototype instruction lookups.

---
+++ findBestMatchTest.py
@@ -0,0 +1,109 @@
+"""
+Tries to lookup a physical instruction based on the string.
+Need to do a lot more here.
+
+    + Better handling of r/m stuff.
+    + Save values so we can actually build the instanciated instruction.
+    + Figure out where we really want this in our file structure.
+"""    
+
+from x86tokenizer import (tokenizeInst,
+                          REGISTER,OPCODE,COMMA,OPERAND,
+                          LBRACKET,RBRACKET,NUMBER,SYMBOL)
+
+from x86inst import mnemonicDict
+
+def possibleDefault(*toks):
+    "By default, a token will just yield itself."
+    first,rest = toks[0],toks[1:]
+    if not rest:
+        yield [first]
+    else:
+        i = 1
+        possibleLookup = getProperLookup(*rest)
+        for restMatches in possibleLookup(*rest):
+            i += 1
+            yldVal = [first]
+            yldVal.extend(restMatches)
+            yield yldVal
+
+def possibleImmediate(*toks):
+    immediateVals = ['imm32','imm16','imm8']
+    first,rest = toks[0],toks[1:]
+    if not rest:
+        for val in immediateVals:
+            yield [(OPERAND, val)]
+    else:
+        possibleLookup = getProperLookup(*rest)
+        for val in immediateVals:
+            for restMatches in possibleLookup(*rest):
+                yldVal = [(OPERAND,val)]
+                yldVal.extend(restMatches)
+                yield yldVal
+
+def possibleRelative(*toks):
+    immediateVals = ['rel32','rel16','rel8']
+    first,rest = toks[0],toks[1:]
+    if not rest:
+        for val in immediateVals:
+            yield [(OPERAND,val)]
+    else:
+        possibleLookup = getProperLookup(*rest)
+        for val in immediateVals:
+            for restMatches in possibleLookup(*rest):
+                yldVal = [(OPERAND,val)]
+                yldVal.extend(restMatches)
+                yield yldVal
+
+def possibleRegister(*toks):
+    """
+    Registers may be hardcoded for superfast lookups, or an r or r/m value.
+    We could probably optimize better with a better understanding of the environment.
+        i.e. it doesn't make sense to move an r/m8 into an r32
+    """    
+    registerVals = [(REGISTER, '%s' % toks[0][1]),
+                    (OPERAND,'r32'),(OPERAND,'r16'),(OPERAND,'r8'),
+                    (OPERAND,'r/m32'),(OPERAND,'r/m16'),(OPERAND,'r/m8')]
+    first,rest = toks[0],toks[1:]
+    if not rest:
+        for val in registerVals:
+            yield [val]
+    else:
+        possibleLookup = getProperLookup(*rest)
+        for val in registerVals:
+            for restMatches in possibleLookup(*rest):
+                yldVal = [val]
+                yldVal.extend(restMatches)
+                yield yldVal
+
+possibleLookups = {
+    REGISTER:possibleRegister,
+    OPCODE:possibleDefault,
+    COMMA:possibleDefault,
+    LBRACKET:possibleDefault,
+    RBRACKET:possibleDefault, # should really never get here
+    NUMBER:possibleImmediate,
+    SYMBOL:possibleDefault,}
+
+def getProperLookup(*toks):
+    return possibleLookups[toks[0][0]]
+
+def findBestMatch(s):
+    retVal = None
+    toks = tokenizeInst(s)
+    for x in possibleDefault(*toks):
+        y = tuple(x)
+        if mnemonicDict.has_key(y):
+            retVal = mnemonicDict[y]
+            break
+    if retVal:
+        return retVal
+    else:
+        raise RuntimeError("Unable to find match for '%s'" % s)
+
+def testBestMatch(s):
+    print "Best match for '%s' => '%s'" % (s,findBestMatch(s))
+
+findBestMatch("MOV EAX, 12")
+findBestMatch("MOV EAX,EBX")
+findBestMatch("MOV [EAX],12")
