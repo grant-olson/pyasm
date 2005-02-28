f1578ee2cf0c97bf40ce8601eca52d6a93c2f3d8
Grant Olson <kgo@grant-olson.net>
2005-02-28 23:13:31 +0000

    more tests

--- test/test_winmem.py
+++ test/test_winmem.py
@@ -1,5 +1,7 @@
 import unittest
 import pyasm.winmem
+from pyasm.x86asm import assembler
+from pyasm.x86cpToMemory import CpToMemory
 
 class test_winmem(unittest.TestCase):
     def test_winmem(self):
@@ -8,8 +10,22 @@ class test_winmem(unittest.TestCase):
         self.assertEquals(cp + 4, pyasm.winmem.GetCurrentExecutablePosition())
         pyasm.winmem.LoadExecutableMemoryString(cp,"aaaa")
         self.assertEquals(cp + 4, pyasm.winmem.GetCurrentExecutablePosition())
-        
 
+    def test_simple_function(self):
+        a = assembler()
+        a.ADStr("hello_world", "Hello world!")
+        a.AP("test_print")
+        a.AddLocal("self")
+        a.AddLocal("args")
+        a.AI("PUSH hello_world")
+        a.AI("CALL PySys_WriteStdout")
+
+        mem = CpToMemory(a.Compile(),pyasm.winmem)
+        mem.MakeMemory(globals())
+
+        print globals()
+        test_print("Foo")
+        
 if __name__ == "__main__":
     unittest.main()
     
