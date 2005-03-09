5a899d7f3a798049a6d7c369d75392456d09b87a
Grant Olson <kgo@grant-olson.net>
2005-03-09 00:07:03 +0000

    hack fixes to get distribution working

--- __init__.py
+++ __init__.py
@@ -1,7 +1,15 @@
 from x86asm import codePackageFromFile
 from x86cpToMemory import CpToMemory
-from pythonConstants import PythonConstants
+#from pythonConstants import PythonConstants
 import cStringIO
+
+def PythonConstants(a):
+    """
+    HACK UNTIL WE FIGURE OUT WHY DISTUTILS WON'T ADD pythonConstants.py
+    takes an assembler object and loads appropriate constants for
+    python
+    """
+    a.AC("PyNone",repr(id(None)))
 
 def pyasm(scope,s):
     cp = codePackageFromFile(cStringIO.StringIO(s),PythonConstants)
