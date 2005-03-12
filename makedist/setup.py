d26efc0ef017d9c915d6e50ceed88a865b78f198
Grant Olson <kgo@grant-olson.net>
2005-03-12 23:20:25 +0000

    Prepping for version 0.3 release

--- makedist/setup.py
+++ makedist/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 excmem = Extension('pyasm.excmem',['pyasm/excmem/excmem.c'])
 
 setup(name='pyasm',
-      version='0.2',
+      version='0.3',
       description='dynamic x86 assembler for python',
       author='Grant Olson',
       author_email='olsongt@verizon.net',
