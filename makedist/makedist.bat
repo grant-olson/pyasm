baef7e9d610e2f6079a40d3b68f2e4723b443e2b
Grant Olson <kgo@grant-olson.net>
2010-03-28 17:50:48 +0000

    Sign packages.

--- makedist/makedist.bat
+++ makedist/makedist.bat
@@ -3,4 +3,9 @@ rmdir /Q /S pyasm
 svn export svn+ssh://grant@johnwhorfin/var/local/svn/pyasm/trunk pyasm
 setup.py sdist --formats=gztar,zip
 setup.py bdist_wininst
-::rmdir /Q /S pyasm
+rmdir /Q /S pyasm
+
+cd dist
+gpg --detach-sign pyasm-0.3.tar.gz
+gpg --detach-sign pyasm-0.3.win32-py2.6.exe
+gpg --detach-sign pyasm-0.3.zip
